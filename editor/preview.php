745c2e3bb4da381dd60b8e38e6ca0a659982eb4f
sappachok <suppachok_sin@nstru.ac.th>
2019-10-16 04:29:28 +0700

    fix css time function

--- editor/preview.php
+++ editor/preview.php
@@ -4,21 +4,26 @@
     <title>Bootstrap Tutor</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="../../assets/bootstrap4/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../../assets/fontawesome5/css/all.min.css">
-    <script src="../../assets/jquery/3.3.1/jquery.min.js"></script>
-    <script src="../../assets/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="../../assets/bootstrap4/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="../../../assets/bootstrap4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../../assets/fontawesome5/css/all.min.css">
+    <script src="../../../assets/jquery/3.3.1/jquery.min.js"></script>
+    <script src="../../../assets/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="../../../assets/bootstrap4/js/bootstrap.min.js"></script>
 </head>
 <body>
 <iframe id="frm_preview" frameborder="0" style="height:480px; width:100%;"></iframe>
 <script>
-$(document).ready(function() {
+jQuery(document).ready(function() {
 	var projectcode = window.parent.get_code();
 	function load_page() {
 		var previewFrame = document.getElementById('frm_preview');
 		var preview = previewFrame.contentDocument || previewFrame.contentWindow.document;	
-		$.get('code.html', function(data) {
+
+		jQuery.get('code.html', function(data) {
+			var dt = new Date()
+			t = dt.getTime()
+			projectcode = projectcode.replace(/{%time}/gi, t);
+			//alert(projectcode);
 			preview.open("text/html", "replace");
 			preview.write(projectcode);
 			preview.close();
